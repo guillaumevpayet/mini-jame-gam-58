afd9180d5c46de84c75fbfdd1992bb06cecdfa67
Zelders56 <Zelders56@gmail.com>
2026-08-23 18:46:43 +0100

    furnace tweens
    added scale and rotation tweens to the combo furnace

--- scripts/furnace.gd
+++ scripts/furnace.gd
@@ -8,11 +8,13 @@ extends AnimatedSprite2D
 @onready var furnace = $"."
 
 var base_scale: Vector2
+var base_rotation: float
 var scale_tween
 var rotation_tween
 
 func _ready() -> void:
 	base_scale = furnace.scale
+	base_rotation = furnace.rotation
 	play()
 
 
@@ -34,19 +36,27 @@ func _on_main_game_scene_combo_changed() -> void:
 		if Globals.current_combo == 0:
 			_audio_stream_player.play()
 		
-		if Globals.current_combo > 6 and scale_tween == null:
-			combo_tweens()
-		elif Globals.current_combo <= 6 and scale_tween != null:
-			scale_tween.kill()
-			scale_tween = null
-			furnace.scale = base_scale
+		if Globals.current_combo > 6:
+			if scale_tween == null:
+				pulse_tween()
+			elif rotation_tween == null:
+				wiggle_tween()
+		elif Globals.current_combo <= 6:
+			if scale_tween != null:
+				scale_tween.kill()
+				scale_tween = null
+				furnace.scale = base_scale
+			elif rotation_tween != null:
+				rotation_tween.kill()
+				scale_tween = null
+				furnace.rotation = base_rotation
 
 	_x.play(str(Globals.current_combo))
 	_number.play(str(Globals.current_combo))
 	_heatwave_material.set_shader_parameter("heat_level", Globals.current_combo)
 
 
-func combo_tweens()-> void:
+func pulse_tween()-> void:
 	scale_tween = create_tween()
 	scale_tween.set_loops()
 	scale_tween.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_LINEAR)
@@ -60,4 +70,28 @@ func combo_tweens()-> void:
 		func(weight: float):
 			furnace.scale = furnace.scale.lerp(base_scale, weight),
 			0.0, 1.0, 0.3)
+
+func wiggle_tween()-> void:
+	rotation_tween = create_tween()
+	rotation_tween.set_loops()
 	
+	rotation_tween.tween_method(
+		func(weight: float):
+			var combo_rotation: float = 0.01 * (Globals.current_combo - 6)
+			furnace.rotation =  combo_rotation * weight,
+			0.0, 1.0, 0.15
+	)
+	
+	rotation_tween.tween_method(
+		func(weight: float):
+			var combo_rotation: float = 0.01 * (Globals.current_combo - 6)
+			furnace.rotation = combo_rotation * weight, 
+			1.0, -1.0, 0.3
+	)
+	
+	var start_rotation: float = furnace.rotation
+	rotation_tween.tween_method(
+		func(weight: float):
+			furnace.rotation = lerp(start_rotation, base_rotation, weight),
+			0.0, 1.0, 0.15
+	)
