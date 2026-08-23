63f1e91a50c6cae84083f810b9a5a7b52a04fbd3
Guillaume Payet <guillaumevpayet@gmail.com>
2026-08-23 18:25:59 +1200

    Added a heatwave shader.

--- scripts/furnace.gd
+++ scripts/furnace.gd
@@ -4,6 +4,7 @@ extends AnimatedSprite2D
 @onready var _audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 @onready var _x: AnimatedSprite2D = $XAnimatedSprite2D
 @onready var _number: AnimatedSprite2D = $NumberAnimatedSprite2D
+@onready var _heatwave_material: Material = material
 
 
 var _combo: int = 0
@@ -11,6 +12,10 @@ var _combo: int = 0
 
 func _ready() -> void:
 	play()
+
+
+func _process(_delta: float) -> void:
+	_heatwave_material.set_shader_parameter("time", Time.get_ticks_msec() / 1000.0)
 
 
 func _on_frame_changed() -> void:
@@ -29,4 +34,5 @@ func _on_main_game_scene_combo_changed(combo: int) -> void:
 
 	_x.play(str(combo))
 	_number.play(str(combo))
+	_heatwave_material.set_shader_parameter("heat_level", combo)
 	_combo = combo
